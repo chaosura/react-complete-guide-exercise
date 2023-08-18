35c66d2dad00fc44e253c8aa1bb61260b8c0532d
clorodyne <clorodyne@iei.or.kr>
2023-08-18 23:04:49 +0900

    I finished this assignment

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filterValue,setFilterValue] = useState();
+  const retreivingFilterValue = (fValue) =>{
+    console.log("Expenses has got this value : " + fValue);
+    setFilterValue(fValue);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter onGettingFilterValue={retreivingFilterValue}/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
--- components/NewExpense/NewExpense.js
+++ components/NewExpense/NewExpense.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
-
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
